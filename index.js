54f522a5b34e646e6f620b6d5fbeee88f1f25a7c
Veronica Bustabad <vbustabad@gmail.com>
2018-09-05 17:54:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function printBadges(employees) {
+  for (let employee_count = 0; employee_count < employees.length; employee_count++) {
+    console.log('Welcome #{employees[employee_count - 1]}! You are employee #{employee_count}.');
+  }
+  return employees;
+}
+
+function tailsCoinFlip() {
+  return Math.random() >= 0.2;
+}
+
+function tailsNeverFails() {
+  while (tailsCoinFlip()) {
+  console.log("You got #{tails_count} tails in a row!");
+  }
+}
