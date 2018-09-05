97be451c6817fde51b6663a34fe3bdbf1896c71f
Veronica Bustabad <vbustabad@gmail.com>
2018-09-05 19:09:56 +0000

    Done.

--- index.js
+++ index.js
@@ -2,17 +2,21 @@
 
 function printBadges(employees) {
   for (let employee_count = 0; employee_count < employees.length; employee_count++) {
-    console.log('Welcome #{employees[employee_count - 1]}! You are employee #{employee_count}.');
+    console.log(`Welcome ${employees[employee_count]}! You are employee #${employee_count + 1}.`);
   }
   return employees;
 }
 
 function tailsCoinFlip() {
-  return Math.random() >= 0.2;
+  return Math.random() >= 0.5;
 }
 
 function tailsNeverFails() {
+  let tails_count = 0;
+
   while (tailsCoinFlip()) {
-  console.log("You got #{tails_count} tails in a row!");
+    tails_count++;
   }
+
+  return `You got ${tails_count} tails in a row!`;
 }
